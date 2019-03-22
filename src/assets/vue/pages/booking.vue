f0d14b457722859a51ad2c4cf20796cb4ed5cdf6
Dilawar Singh <dilawars@ncbs.res.in>
2019-03-22 21:14:01 +0530

    Datepicker is woriing fine.

--- src/assets/vue/pages/booking.vue
+++ src/assets/vue/pages/booking.vue
@@ -7,19 +7,17 @@
     <!--  Show venues as grid or list. -->
     <f7-page-content>
 
-      <!-- Date and time -->
-      <f7-block>
-        <f7-row>
-          <f7-col> 
-              <f7-input name="date" type="date" label="Date"></f7-input>
-          </f7-col>
-          <f7-col>
-              <f7-input name="time" type="time" label="Time"></f7-input>
-          </f7-col>
-        </f7-row>
-      </f7-block>
       <f7-block>
           <f7-button raised fill round @click="onPageInit">Refresh venues</f7-button>
+      </f7-block>
+
+      <!-- Date and time -->
+      <f7-block>
+        <date-picker v-model="searchVenueAt" type="datetime" 
+                     format="YYYY-MM-DD hh:mm" 
+                     :minute-step="10"
+                     lang="en" confirm >
+        </date-picker>
       </f7-block>
 
       <f7-block>
@@ -37,51 +35,35 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        venues : null,
-        showPreloader: true,
-      };
-    },
-    methods: {
-      onPageInit : function() {
-        console.log("Fetching all venues");
-        const self = this;
-        const app = self.$f7;
 
-        // Try to connect.
-        app.request.post(self.$store.state.api + '/venue/list/all'
-          , {'HIPPO-API-KEY': self.$localStorage.get('HIPPO-API-KEY')
-            , 'login': self.$localStorage.get('HIPPO-LOGIN') 
-          }, 
-          function(json) {
-            var res = JSON.parse(json);
-            if(res.status == 'ok')
-              self.venues = res.data;
-          });
-      }
-    },
+import DatePicker from 'vue2-datepicker'
 
-    //on: { 
-    //  pageInit(page) {
-    //    console.log( "Fething all venues" );
-    //    //const self = this;
-    //    //setTimeout( () => {
-    //    //  const app = self.$f7;
+export default {
+  components : {DatePicker},
+  data() {
+    return {
+      venues : null,
+      showPreloader: true,
+      searchVenueAt : '',
+    };
+  },
+  methods: {
+    onPageInit : function() {
+      console.log("Fetching all venues");
+      const self = this;
+      const app = self.$f7;
 
-    //    //  // Try to connect.
-    //    //  app.request.post(self.$store.state.api + '/venue/list/all'
-    //    //    , {'HIPPO-API-KEY': self.$localStorage.get('HIPPO-API-KEY')
-    //    //      , 'login': self.$localStorage.get('HIPPO-LOGIN') 
-    //    //    }, 
-    //    //    function(json) {
-    //    //      var res = JSON.parse(json);
-    //    //      console.log(res);
-    //    //    });
-    //    //  done();
-    //    //}, 1000);
-    //  },
-    //},
-  };
+      // Try to connect.
+      app.request.post(self.$store.state.api + '/venue/list/all'
+        , {'HIPPO-API-KEY': self.$localStorage.get('HIPPO-API-KEY')
+          , 'login': self.$localStorage.get('HIPPO-LOGIN') 
+        }, 
+        function(json) {
+          var res = JSON.parse(json);
+          if(res.status == 'ok')
+            self.venues = res.data;
+        });
+    }
+  },
+};
 </script>
