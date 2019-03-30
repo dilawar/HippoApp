fa00a5f3e1742c79197133998330b2b002b857df
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-03-30 08:54:03 +0530

    Minor cleanup.

--- src/assets/vue/pages/mybooking.vue
+++ src/assets/vue/pages/mybooking.vue
@@ -1,114 +1,67 @@
 <template>
-
-  <f7-page @page:init="fetchMyBooking" @page:refresh="fetchMyBooking">
+  <f7-page ptr @ptr:refresh="refreshMyBooking" @page:init="fetchMyBooking">
   <f7-navbar title="My Bookings" back-link="Back"></f7-navbar>
-
-  <!--  Show venues as grid or list. -->
   <f7-page-content>
-
-    <!-- Date and time -->
-    <f7-list no-hairlines simple-list inset>
-       <f7-list-item>
-          <date-picker v-model="startDate" 
-                       lang="en"
-                       format="MMM DD, YYYY"
-                       append-to-body
-                       :popupStyle="{'z-index':10000}"
-                       type="date"> 
-          </date-picker>
-         <f7-button title="Referesh" 
-                    filled raised
-                    @click="fetchMyBooking">Refresh</f7-button>
-       </f7-list-item>
-    </f7-list>
-
-    <f7-block-title medium>Pending requests</f7-block-title>
-    <f7-list>
-       <f7-list-item accordion-item
-               v-for="(requests, gid, index) in requestGroups" 
-               :title="requests[0].title"
-               :key="gid"
-               :footer="requests[0].venue" >
-          <f7-accordion-content>
-             <f7-list>
-                <f7-list-item swipeout
+     <f7-block>
+        <f7-list media-list no-hairlines>
+           <f7-list-item accordion-item
+                         v-for="(requests, gid, index) in requestGroups" 
+                         :title="requests[0].title"
+                         :key="gid"
+                         :footer="requests[0].venue">
+                <f7-accordion-content>
+                   <f7-list>
+                      <f7-list-item swipeout
                            @swipeout:deleted="deleteThisRequest(val.gid, val.rid)"
                            v-for="(val, index) in requests" 
                            :key="val.gid+'.'+val.rid" 
-                           :title="val.date+', '+val.start_time+' (' + val.venue + ')'"
-                           >
-                   <f7-icon slot=media icon="fa fa-hourglass-half"></f7-icon>
-                   <f7-swipeout-actions right>
-                      <f7-swipeout-button delete
-                                        title="Deleting this request?" delete 
-                                        confirm-text="Are you sure you want to delete this request?"
-                        >Delete</f7-swipeout-button>
-                   </f7-swipeout-actions>
-                </f7-list-item>
-             </f7-list>
-          </f7-accordion-content>
-       </f7-list-item>
-    </f7-list>
+                           :title="val.date+', '+val.start_time+' (' + val.venue + ')'">
+                           <f7-icon slot=media icon="fa fa-hourglass-half"></f7-icon>
+                           <f7-swipeout-actions right>
+                              <f7-swipeout-button delete
+                                                  title="Deleting this request?" delete 
+                                                  confirm-text="Are you sure you want to delete this request?"
+                                                  >Delete
+                              </f7-swipeout-button>
+                           </f7-swipeout-actions>
+                      </f7-list-item>
+                   </f7-list>
+                </f7-accordion-content>
+           </f7-list-item>
 
-    <f7-block-title medium>Approved booking</f7-block-title>
-    <f7-list accordion-list >
-       <f7-list-item accordion-item 
-               v-for="(events, gid, index) in eventGroups" 
-               :title="events[0].title"
-               :footer="events[0].venue" 
-               :key="gid"
-               >
-            <f7-accordion-content>
-               <!-- NOTE: Only simple-list plays well with nested swipeout -->
-               <f7-list media-list>
-                  <!-- Swipeout on individual element. -->
-                  <f7-list-item swipeout 
-                                v-for="(val, index) in events" 
-                                @swipeout:deleted="deleteEvent(val.gid, val.eid)"
-                                :key="val.gid+'.'+val.eid" 
-                                :title="val.date+', '+val.start_time"
-                                :footer="val.venue + ' | ' + val.title"
-                                >
-                     <f7-icon slot="media" icon="fa fa-check-circle fa-2x"></f7-icon>
-                     <f7-swipeout-actions right>
-                        <f7-swipeout-button delete
-                                            color="blue"
-                                            title="Deleting request?" 
-                                            confirm-text="Delete this item?"
-                                            >Delete</f7-swipeout-button>
-                     </f7-swipeout-actions>
-                  </f7-list-item>
-               </f7-list>
-            </f7-accordion-content>
-       </f7-list-item>
-    </f7-list>
-
-
-
-          <!--
-
-       <f7-list-item v-for="(val, items) in requests" 
-                     swipeout  
-                     @swipeout:deleted="deleteThisRequest(val.gid, val.rid)"
-                     :footer="val.date+', '+val.start_time+' (' + val.venue + ')'"
-                     :key="val.gid+val.rid"
-                     :title="val.title"
-                     >
-         <f7-icon slot=media icon="fa fa-hourglass-half"></f7-icon>
-         <f7-swipeout-actions right>
-            <f7-swipeout-button title="Deleting request?" delete 
-               confirm-text="Are you sure you want to delete this item?">Delete</f7-swipeout-button>
-         </f7-swipeout-actions>
-         <f7-swipeout-actions left>
-            <f7-swipeout-button edit 
-                                @click="editThisRequest(val.gid, val.rid)"
-                               >Edit</f7-swipeout-button>
-         </f7-swipeout-actions>
-       </f7-list-item>
-          -->
-
+           <f7-list-item accordion-item 
+                         v-for="(events, gid, index) in eventGroups" 
+                         :title="events[0].title"
+                         :footer="events[0].venue" 
+                         :key="gid"
+                         >
+                         <f7-accordion-content>
+                            <!-- NOTE: Only simple-list plays well with nested swipeout -->
+              <f7-list media-list>
+                 <!-- Swipeout on individual element. -->
+                 <f7-list-item swipeout 
+                               v-for="(val, index) in events" 
+                               @swipeout:deleted="deleteEvent(val.gid, val.eid)"
+                               :key="val.gid+'.'+val.eid" 
+                               :title="val.date+', '+val.start_time"
+                               :footer="val.venue + ' | ' + val.title"
+                               >
+                               <f7-icon slot="media" icon="fa fa-check-circle fa-2x"></f7-icon>
+                               <f7-swipeout-actions right>
+                                  <f7-swipeout-button delete
+                                                      color="blue"
+                                                      title="Deleting request?" 
+                                                      confirm-text="Delete this item?"
+                                                      >Delete</f7-swipeout-button>
+                               </f7-swipeout-actions>
+                 </f7-list-item>
+              </f7-list>
+                         </f7-accordion-content>
+           </f7-list-item>
+        </f7-list>
+     </f7-block>
   </f7-page-content>
-</f7-page>
+  </f7-page>
 
 </template>
 
@@ -133,7 +86,8 @@ export default {
          self.isOpen        = false;
 
 
-         var groupBy = function(xs, key) {
+         var groupBy = function(xs, key) 
+         {
             return xs.reduce(function(rv, x) {
                (rv[x[key]] = rv[x[key]] || []).push(x);
                return rv;
@@ -141,15 +95,23 @@ export default {
          };
 
          var link = self.$store.state.api+'/mybooking/list/'+moment(self.startDate).format('X');
-         console.log('Link is', link);
          app.request.post(link, this.apiPostData(),
-            function(json) {
+            function(json) 
+            {
                var res = JSON.parse(json);
                self.requestGroups = res.data.requests;
                self.eventGroups = res.data.events;
-               //console.log( 'events', self.eventGroups );
             }
          );
+      },
+      refreshMyBooking: function(event, done) 
+      {
+         console.log("Refreshing booking request.");
+         const self = this;
+         setTimeout(() => {
+            self.fetchMyBooking();
+            done();
+         }, 1000);
       },
       deleteEvent: function(gid, eid) 
       {
