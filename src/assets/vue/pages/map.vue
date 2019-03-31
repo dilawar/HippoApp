80a05df320018a9d32af831f92dc1e911809dc9b
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-03-31 14:29:45 +0530

    Added map.

--- src/assets/vue/pages/map.vue
+++ src/assets/vue/pages/map.vue
@@ -1,42 +1,56 @@
 <template>
-<f7-page page-content ptr @ptr:refresh="fetchMap" >
-  <f7-navbar title="Map" back-link="Back"></f7-navbar>
+   <f7-page page-content>
+      <f7-navbar title="Map" back-link="Back"></f7-navbar>
 
-  <!-- Days related -->
-  <f7-block>
-     <no-ssr>
-        <l-map :zoom="zoom" :center="center">
-           <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-           <l-marker :lat-lng="marker"></l-marker>
-        </l-map>
-     </no-ssr>
-  </f7-block>
+      <l-map ref="map" :zoom="zoom" 
+             :center="center"
+             :style="mapStyle"
+             @update:center="centerUpdated"
+             @update:zoom="zoomUpdated"
+             @update:bounds="boundsUpdated"
+             >
+             <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      </l-map>
 
-</f7-page>
+   </f7-page>
 </template>
 
 <script>
-
-import moment from 'moment';
-moment.defaultFormat = 'YYYY-MM-DD';
-
 export default {
    data() {
       return {
-         zoom:20,
-         center: L.latLng(13.0714215, 77.5810642),
+         zoom:17,
+         bounds: null,
+         center: L.latLng(13.071081, 77.58025),
          url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
          attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-         marker: L.latLng(13.0714215, 77.5810642),
+         mapStyle: 'width:100%; height:100%',
+         venues: [ 
+            { xy: L.latLng(13.0714215, 77.5810642), 'name' : 'Reception'},
+            { xy: L.latLng(13.071081, 77.58025), 'name' : 'SLC'},
+         ],
       };
    },
    computed: {
    },
-   mounted: function() {
-      const self = this;
+   mounted () {
+      this.$refs.map.mapObject.invalidateSize();
    },
    methods: { 
-      fetchMap: function() {
+      onResize: function() {
+      },
+      refreshMap: function() {
+         const map = this.$refs.map.mapObject;
+         //this.venues.map( x => x );
+      },
+      zoomUpdated (zoom) {
+         this.zoom = zoom;
+      },
+      centerUpdated (center) {
+         this.center = center;
+      },
+      boundsUpdated (bounds) {
+         this.bounds = bounds;
       },
    },
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // Import Vue
 import Vue from 'vue'
 
-// OSM.
+// OSM and leaflet.
 import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
 Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
 Vue.component('l-marker', LMarker);
