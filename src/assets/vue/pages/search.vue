b5edb0f1a6e99880f4f1fdcf5a857ba7908fd9c1
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-04-03 00:22:29 +0530

    Time to realse v1.0.8

--- src/assets/vue/pages/search.vue
+++ src/assets/vue/pages/search.vue
@@ -1,8 +1,8 @@
 <template>
    <f7-page page-content>
       <f7-navbar title="Search" back-link="Back"></f7-navbar>
-
-      <f7-list media-list no-hairlines inset>
+      <f7-block>
+         <f7-list simple-list>
          <f7-list-item>
             <f7-list-input v-model="query" 
                            @input="query=$event.target.value"
@@ -13,27 +13,27 @@
          </f7-list-item>
       </f7-list>
 
-      <f7-block>
-         <f7-list media-list>
-            <f7-list-item v-for="(e,key) in ldapInfo" 
-                          :key="key"
-                          :after="e.extension"
-                          :footer="`${e.group}`"
-                          >
-                 <f7-link slot="title" 
-                          external
-                          icon="fa fa-envelope-o fa-fw"
-                          :href="`mailto:${e.email}`"
-                          target="_blank"
-                          > {{e.name}} </f7-link>
-                 <f7-link slot="after" 
-                          external
-                          icon="fa fa-phone fa-fw"
-                          :href="`tel:${e.phone}`"
-                          target="_blank"
-                          > {{e.phone}} </f7-link>
-            </f7-list-item>
-         </f7-list>
+      <f7-list media-list>
+         <f7-list-item v-for="(e,key) in ldapInfo" 
+                       :key="key"
+                       :after="e.extension"
+                       :footer="`${e.group}`"
+                       >
+                       <f7-link slot="title" 
+                                external
+                                icon="fa fa-envelope-o fa-fw"
+                                :href="`mailto:${e.email}`"
+                                target="_blank"
+                                > <small>{{e.name}}</small></f7-link>
+                       <f7-link slot="after" 
+                                external
+                                icon="fa fa-phone fa-fw"
+                                :href="`tel:${e.phone}`"
+                                target="_blank"
+                                > <small> {{e.phone}}</small> </f7-link>
+         </f7-list-item>
+      </f7-list>
+
       </f7-block>
 
    </f7-page>
@@ -61,12 +61,15 @@ export default {
          app.request.post(link, this.apiPostData(),
             function(json) {
                var ldap = JSON.parse(json).data;
-               self.ldapInfo = ldap;
+
+               // Keep a copy else it will clutter.
+               self.ldapInfo =JSON.parse(json).data;
 
                // Append to my personal library.
                var stored = JSON.parse(self.$localStorage.get('ldap.cache'), '[]');
-               if(stored)
-                  ldap = ldap.push(...stored);
+               console.log('stored', stored);
+               for(var k in stored)
+                  ldap.push(stored[k]);
                self.$localStorage.set('ldap.cache', JSON.stringify(ldap));
             }
          );
