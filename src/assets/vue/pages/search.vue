3beb61abf1c03173e43f0d4e5d978439a79cad7f
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-04-03 00:05:10 +0530

    Search is working fine.

--- src/assets/vue/pages/search.vue
+++ src/assets/vue/pages/search.vue
@@ -1,36 +1,42 @@
 <template>
-<f7-page>
-  <f7-navbar title="Search" back-link="Back"></f7-navbar>
-  <!--  Show venues as grid or list. -->
-  <f7-page-content>
+   <f7-page page-content>
+      <f7-navbar title="Search" back-link="Back"></f7-navbar>
 
-     <f7-list media-list no-hairlines inset>
-        <f7-list-item>
-           <f7-list-input v-model="query" 
-                          @input="query=$event.target.value"
-                          type="text" 
-                          placeholder="Search NCBS"></f7-list-input>
-           <f7-button @click="searchIntranet" raised fill round>Search</f7-button>
-        </f7-list-item>
-     </f7-list>
+      <f7-list media-list no-hairlines inset>
+         <f7-list-item>
+            <f7-list-input v-model="query" 
+                           @input="query=$event.target.value"
+                           type="text" 
+                           placeholder="Search NCBS">
+            </f7-list-input>
+            <f7-button @click="searchIntranet" raised fill round>Search</f7-button>
+         </f7-list-item>
+      </f7-list>
 
-     <f7-list media-list no-hairlines>
-        <f7-list-item v-for="(val, key, item) in user" 
-                      :key="key"
-                      :header="key" 
-                      :title="val">
-           <f7-icon slot="media" 
-                    v-if="key.toLowerCase()=='extenstion'" 
-                    icon="fa fa-phone fa-2x"></f7-icon>
-           <f7-icon slot="media" v-else></f7-icon>
-           <a slot="after" 
-              v-if="key.toLowerCase()=='extenstion'" 
-              href="tel:+91-802363${val}">+91 80 2362{{val}}</a>
-        </f7-list-item>
-     </f7-list>
+      <f7-block>
+         <f7-list media-list>
+            <f7-list-item v-for="(e,key) in ldapInfo" 
+                          :key="key"
+                          :after="e.extension"
+                          :footer="`${e.group}`"
+                          >
+                 <f7-link slot="title" 
+                          external
+                          icon="fa fa-envelope-o fa-fw"
+                          :href="`mailto:${e.email}`"
+                          target="_blank"
+                          > {{e.name}} </f7-link>
+                 <f7-link slot="after" 
+                          external
+                          icon="fa fa-phone fa-fw"
+                          :href="`tel:${e.phone}`"
+                          target="_blank"
+                          > {{e.phone}} </f7-link>
+            </f7-list-item>
+         </f7-list>
+      </f7-block>
 
-  </f7-page-content>
-</f7-page>
+   </f7-page>
 </template>
 
 <script>
@@ -39,9 +45,11 @@ moment.defaultFormat = 'YYYY-MM-DD';
 
 export default {
    data() {
+      const self = this;
       return {
          query: null,
-         user: [],
+         ldapCache: JSON.parse(self.$localStorage.get('ldap.cache', '[]')),
+         ldapInfo : [],
       };
    },
    methods: { 
@@ -52,10 +60,16 @@ export default {
          var link = self.$store.state.api+'/ldap/'+self.query;
          app.request.post(link, this.apiPostData(),
             function(json) {
-               var res = JSON.parse(json);
-               self.user = res.data;
-               console.log(self.user);
-            });
+               var ldap = JSON.parse(json).data;
+               self.ldapInfo = ldap;
+
+               // Append to my personal library.
+               var stored = JSON.parse(self.$localStorage.get('ldap.cache'), '[]');
+               if(stored)
+                  ldap = ldap.push(...stored);
+               self.$localStorage.set('ldap.cache', JSON.stringify(ldap));
+            }
+         );
       },
    },
 };
