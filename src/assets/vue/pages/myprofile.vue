3906670ac58954091d1f3bed73b63fc9ab7e4431
Dilawar Singh <dilawars@ncbs.res.in>
2019-04-04 00:48:31 +0530

    profile is added to Hippo.

--- src/assets/vue/pages/myprofile.vue
+++ src/assets/vue/pages/myprofile.vue
@@ -1,5 +1,5 @@
 <template>
-   <f7-page page-content @page:init="reinit">
+   <f7-page page-content ptr @ptr:refresh="fetchProfile">
       <f7-navbar title="Profile" back-link="Back"></f7-navbar>
 
       <f7-block>
@@ -9,12 +9,23 @@
       </f7-block>
 
       <!-- FAB Right Bottom (Blue) -->
-      <f7-fab v-if="alreadyLoggedIn" position="right-bottom" slot="fixed" color="blue">
+      <f7-fab position="right-bottom" slot="fixed" color="blue">
          <f7-icon ios="f7:add" aurora="f7:add" md="material:add"></f7-icon>
          <f7-icon ios="f7:close" aurora="f7:close" md="material:close"></f7-icon>
          <f7-fab-buttons position="top">
          </f7-fab-buttons>
       </f7-fab>
+
+      <f7-block>
+         <f7-list>
+            <f7-list-item
+               v-for="(val, key) in profile"
+               :header="key"
+               :title="val"
+               >
+            </f7-list-item>
+         </f7-list>
+      </f7-block>
 
    </f7-page>
 
@@ -25,27 +36,32 @@
       data() {
          const self = this;
          return {
-            profile: JSON.parse(self.$localStorage.get('my.profile', '[]')),
+            profile: [],
          };
       },
       mounted()
       {
          const self = this;
          const app = self.$f7;
-         var $$ = this.$$;
-
-         // Fetch the transport as well.
-         app.request.post( self.$store.state.api+'/me/profile'
-            , self.apiPostData()
-            , function(json) 
-            {
-               const res = JSON.parse(json);
-               if( res.status=='ok')
-                  self.$localStorage.set('me.profile', JSON.stringify(res.data));
-            }
-         );
+         self.fetchProfile( );
+         console.log( 'profile', self.profile);
       },
       methods: {
+         fetchProfile: function() {
+            // Fetch the transport as well.
+            const self = this;
+            const app = self.$f7;
+
+            app.request.post( self.$store.state.api+'/me/profile'
+               , self.apiPostData()
+               , function(json) 
+               {
+                  const res = JSON.parse(json);
+                  if( res.status=='ok')
+                     self.profile = res.data;
+               }
+            );
+         },
       },
    }
 </script>
