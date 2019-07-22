73fd410ab9e10046e4152aa4468b48a6af12e2fc
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-07-22 16:30:33 +0530

    This is not compiling anymore.

--- src/routes.js
+++ src/routes.js
@@ -23,7 +23,6 @@ import Chat from './assets/vue/pages/chat.vue';
 import SearchPage from './assets/vue/pages/search.vue';
 import Vuex from './assets/vue/pages/vuex.vue';
 import InventoryPage from './assets/vue/pages/inventory.vue';
-import QrPage from './assets/vue/pages/qr.vue'
 
 export default [
    {
