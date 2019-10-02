0ccaa74ba0a23d8f90c36b59d8a202f6be5296ba
Dilawar Singh <dilawars@ncbs.res.in>
2019-10-02 21:36:19 +0530

    Started working on ADMIN interface.

--- src/assets/vue/pages/panel-right.vue
+++ src/assets/vue/pages/panel-right.vue
@@ -3,80 +3,22 @@
       <f7-navbar title="Hippo"></f7-navbar>
       <!-- More information here -->
 
-         <f7-block-title>Welcome Admin ({{username}})</f7-block-title>
+      <f7-block v-if="getRoles().includes('BOOKMYVENUE_ADMIN')">
+      <f7-block-title>BookMyVenue Admin</f7-block-title>
+      </f7-block>
 
-         <f7-list no-hairlines>
+      <f7-block v-if="getRoles().includes('ACAD_ADMIN')">
+      <f7-block-title>Acad Admin</f7-block-title>
+      </f7-block>
 
-            <f7-list-item link="/notifications/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="Notifications"
-                          panel-close
-                          >
-               <span slot="media" class="fa-stack" v-if="notifications.length>=0">
-                  <i class="fa fa-bell-o fa-stack-2x"></i>
-                  <i class="fa fa-stack circle-red">{{notifications.length}}</i>
-               </span>
-            </f7-list-item>
+      <f7-block v-if="getRoles().includes('SERVICES_ADMIN')">
+      <f7-block-title>Services Admin</f7-block-title>
+      </f7-block>
 
-            <f7-list-item link="/myprofile/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="My Profile" 
-                          panel-close
-                          >
-               <f7-icon slot="media" icon="fa fa-user fa-2x"></f7-icon>
-            </f7-list-item>
+      <f7-block v-if="getRoles().includes('ADMIN')">
+      <f7-block-title>Hippo Admin</f7-block-title>
+      </f7-block>
 
-            <f7-list-item link="/mybooking/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="My Bookings" 
-                          panel-close
-                          >
-               <f7-icon slot="media" icon="fa fa-edit fa-2x"></f7-icon>
-            </f7-list-item>
-
-            <f7-list-item v-if="profile.eligible_for_aws"
-                          link="/myaws/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="My AWS" 
-                          panel-close
-                          >
-                <f7-icon slot="media" icon="fa fa-graduation-cap fa-2x"></f7-icon>
-            </f7-list-item>
-
-            <f7-list-item link="/myjc/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="Journal Clubs" 
-                          panel-close
-                          >
-                <f7-icon slot="media" icon="fa fa-users fa-2x"></f7-icon>
-            </f7-list-item>
-
-            <f7-list-item link="/mycourse/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="My Courses" 
-                          panel-close
-                          >
-             <f7-icon slot="media" icon="fa fa-book fa-2x"></f7-icon>
-            </f7-list-item>
-
-
-            <f7-list-item link="/myinventory/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="Lab Inventory"
-                          panel-close
-                          >
-               <f7-icon slot="media" icon="fa fa-archive fa-2x"></f7-icon>
-            </f7-list-item>
-
-
-         </f7-list>
 
    </f7-page>
 </template>
