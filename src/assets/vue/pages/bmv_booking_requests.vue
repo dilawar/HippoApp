cedeeb52223758a1882b9dc475e24a9f6ca396d9
Dilawar Singh <dilawars@ncbs.res.in>
2019-10-04 22:24:23 +0530

    Show booking request.

--- src/assets/vue/pages/bmv_booking_requests.vue
+++ src/assets/vue/pages/bmv_booking_requests.vue
@@ -4,18 +4,21 @@
       </f7-navbar>
 
       <f7-block-title small>Booking requests</f7-block-title>
-
       <f7-block v-if="getRoles().includes('BOOKMYVENUE_ADMIN')">
-
-        <f7-list no-hairlines media-list>
-          <f7-list-item v-for="(request, id) in requests">
-            <div slot="header">
-              {{request.venue}} |
-              {{request.date}} |
-              {{request.start_time}}  to {{request.end_time}}
-            </div>
+        <f7-list no-hairlines media-list accordion-list>
+          <f7-list-item v-for="(request, id) in requests"
+                        accordion-item>
+            <div slot="header"> {{request.venue}} | {{request.class}} </div>
+            <div slot="header"> Created by {{request.created_by}} </div>
             <div slot="title"> {{request.title}} </div>
-            <div slot="footer"> {{request.created_by}} </div>
+            <div slot="footer">
+              {{request.date | date}} |
+              {{request.start_time | clockTime}} to 
+              {{request.end_time | clockTime }}
+            </div>
+            <f7-accordion-content>
+               <div v-html="request.description"> </div>
+            </f7-accordion-content>
           </f7-list-item>
         </f7-list>
 
