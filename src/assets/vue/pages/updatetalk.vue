be4d402e1ebe07c628b7968b992a221119f103a0
Dilawar Singh <dilawars@ncbs.res.in>
2019-10-19 23:06:09 +0530

    Some more updated.

--- src/assets/vue/pages/updatetalk.vue
+++ src/assets/vue/pages/updatetalk.vue
@@ -3,7 +3,6 @@
     <f7-navbar title="title" back-link="Back">
     </f7-navbar>
 
-    <!--
     <f7-block>
       <f7-list>
         <f7-list-group media-list no-hairlines>
@@ -11,9 +10,18 @@
                         :title="thisTalk.speaker"
                         footer="You can't change the speaker">
           </f7-list-item>
-          <f7-list-input label="Class" 
-                         :title="thisTalk.class">
-          </f7-list-input>
+          <f7-list-item label="Class" 
+                        :title="thisTalk.class"
+                        :smart-select-params="{openIn:'popover', closeOnSelect:true}"
+                        smart-select>
+            <select v-model="thisTalk.class">
+              <option v-for="(cls, key) in classes" 
+                      :value="cls"
+                      :key="key">
+              {{cls}}
+              </option>
+            </select>
+          </f7-list-item>
           <f7-list-input label="Title" 
                          type="textarea" 
                          :value="thisTalk.title">
@@ -24,19 +32,34 @@
           </f7-list-input>
         </f7-list-group>
 
+        <f7-list-group v-if="thisTalk.booking" media-list>
+          <f7-list-item :header="thisTalk.booking_status">
+            <div slot="text" v-if="thisTalk.booking">
+              {{ thisTalk.booking.date | date }},
+              {{ thisTalk.booking.start_time | clockTime }} to
+              {{thisTalk.booking.end_time | clockTime}},
+              {{ thisTalk.booking.venue }}
+            </div>
+
+            <!-- TASKS -->
+            <f7-button raised small
+                       color="red" slot="footer" class="float-right" v-if="thisTalk.booking">
+              Remove Booking
+            </f7-button>
+            <f7-button raised small class="float-right" slot="footer" v-else>
+              Schedule
+            </f7-button>
+          </f7-list-item>
+
+          <f7-list-item>
+            <f7-button @click="updateTalk()" popup-close raised fill slot="after">
+              Update This Talk
+            </f7-button>
+          </f7-list-item>
+
+        </f7-list-group>
       </f7-list>
-
-      <f7-row>
-        <f7-col>
-          <f7-button popup-close raised outline>
-          </f7-button>
-        </f7-col>
-      </f7-row>
     </f7-block>
-    -->
-
-  </f7-page>
-  </f7-popup>
   </f7-page>
 </template>
 
@@ -45,13 +68,19 @@
     data() {
       const self = this;
       return {
-        talkid : self.$f7router.params.talkid,
+        talkid : self.$f7route.params.talkid,
+        classes: [],
         thisTalk: [],
       };
     },
     mounted()
     {
       const self = this;
+      console.log(self.$f7route.params);
+      self.promiseWithAuth('info/bmv/bookingclasses')
+        .then( function(x) {
+          self.classes = JSON.parse(x.data).data.public;
+        });
       self.promiseWithAuth('talk/get/'+self.talkid)
         .then( function(x) {
           self.thisTalk = JSON.parse(x.data).data;
@@ -60,6 +89,14 @@
     methods : {
       refreshData: function( ) {
         const self = this;
+      },
+      updateTalk: function()
+      {
+        const self = this;
+        self.promiseWithAuth('talk/update', self.thisTalk)
+          .then( function(x) {
+            var res = JSON.parse(x.data).data;
+          });
       },
     }
   }
