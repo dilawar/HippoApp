9a96d5963dbad1e7c7a2e682ce82ab4d9347d2db
Dilawar Singh <dilawars@ncbs.res.in>
2019-10-21 19:13:45 +0530

    Update.

--- src/assets/vue/pages/updatecourse.vue
+++ src/assets/vue/pages/updatecourse.vue
@@ -3,17 +3,54 @@
       <f7-navbar title="Manage Registration/Grade" back-link="Back">
       </f7-navbar>
 
-      <f7-block>
+      <!-- ASSIGN NEW STUDENT -->
+      <f7-block strong>
         <f7-block-title>{{thisCourseId}}. Total {{registrations.length}}
           registrations.
         </f7-block-title>
 
+        <f7-card :padding="false" :outline="false" no-border>
+          <f7-card-content>
+            <f7-list no-hairlines media-list>
+              <f7-list-item header="New registration">
+                <f7-row slot="title">
+                  <f7-col>
+                    <f7-list-input type="text" 
+                                   label="Login"
+                                   @input="thisRegistration.student_id=$event.target.value"
+                                   :value="thisRegistration.student_id"
+                                   >
+                    </f7-list-input>
+                  </f7-col>
+                  <f7-col>
+                    <f7-list-input label="Type" 
+                                   type="select"
+                                   @change="thisRegistration.type=$event.target.value"
+                                   :value="thisRegistration.type">
+                      <option value="CREDIT" selected>CREDIT</option>
+                      <option value="AUDIT">AUDIT</option>
+                    </f7-list-input>
+                  </f7-col>
+                </f7-row>
+                <f7-button small 
+                           slot="after"
+                           @click="addRegistration()"
+                           :disabled="thisRegistration.student_id.length<2">
+                  Register
+                </f7-button>
+              </f7-list-item>
+            </f7-list>
+          </f7-card-content>
+        </f7-card>
+
         <f7-list media-list>
           <f7-list-item v-for="(st,key) in registrations"
+                        :key="key"
                         :title="st.student_id"
                         :after="st.type"
                         @click="onRegSelect(st)"
                         :footer="'Registered on: '+st.registered_on">
+            <div slot="media">{{(st.grade||'X')==='X'?'':st.grade}}</div>
           </f7-list-item>
 
         </f7-list>
@@ -27,15 +64,15 @@
               <f7-link popup-close>Close</f7-link>
             </f7-nav-right>
           </f7-navbar>
-      
-          <f7-block inset>
 
+          <f7-block>
             <f7-block-title>
-              Student ID: {{thisRegistration.student_id}} 
+              {{thisRegistration.student_id}}, {{thisCourseId}}
             </f7-block-title>
 
-              <f7-list>
-                <f7-list-group media-list>
+            <f7-card>
+              <f7-card-content>
+                <f7-list media-list>
                   <f7-list-input label="Registration type" 
                                  type="select"
                                  @change="thisRegistration.type=$event.target.value"
@@ -47,30 +84,45 @@
 
                   <f7-list-item>
                     <f7-row>
-                    <f7-col>
-                      <f7-button color="red" fill @click="dropCourse()">Drop</f7-button>
-                    </f7-col>
-                    <f7-col>
-                      <f7-button @click="changeRegistration()">Change</f7-button>
-                    </f7-col>
+                      <f7-col>
+                        <f7-button color="red" fill @click="dropCourse()">Drop</f7-button>
+                      </f7-col>
+                      <f7-col>
+                        <f7-button @click="changeRegistration()">Change</f7-button>
+                      </f7-col>
                     </f7-row>
                   </f7-list-item>
-                </f7-list-group>
+                </f7-list>
+              </f7-card-content>
+            </f7-card>
 
-                <f7-list-group>
+            <f7-card>
+              <f7-card-content>
+                <f7-list media-list>
                   <f7-list-input label="Grade" 
                                  type="select"
                                  @change="thisRegistration.grade=$event.target.value"
                                  :value="thisRegistration.grade">
-                    <option v-for="g in availableGrades" :value="g">{{g}}</option>
+                    <option v-for="(gr, kye) in availableGrades" :value="gr">
+                      {{gr}}
+                    </option>
                   </f7-list-input>
-                </f7-list-group>
-              </f7-list>
+                  <f7-list-item>
+                    <div slot="header">
+                      To remove existing grade, select 'X'.
+                    </div>
+                    <f7-button slot="after"
+                               :disabled="! thisRegistration.grade"
+                               @click="assignGrade()"
+                      >Assign</f7-button>
+                  </f7-list-item>
+                </f7-list>
+              </f7-card-content>
+            </f7-card>
 
           </f7-block>
         </f7-page>
       </f7-popup>
-
    </f7-page>
 
 </template>
@@ -81,12 +133,12 @@
       const self = this;
       return {
         thisCourseId: self.$f7route.params.courseid,
-        thisCourseMetadata: [],
-        registrations: [],
-        thisRegistration: [],
-        thisData: [],
+        thisCourseMetadata: {},
+        registrations: {},
+        thisRegistration: {student_id:'', type:'CREDIT', grade:''},
+        thisData: {},
         openPopup: false,
-        availableGrades: ['A +','A', 'B+', 'B', 'C+', 'C', 'F', 'X'],
+        availableGrades: ['A+','A', 'B+', 'B', 'C+', 'C', 'F', 'X'],
       };
     },
     mounted()
@@ -127,21 +179,57 @@
           , function(x) {
             self.promiseWithAuth('acadadmin/course/registration/drop', self.thisRegistration)
               .then( function(x) {
-                self.openPopup = false;
                 self.fetchRegistration();
               });
           }, null);
-
+        self.openPopup = false;
       },
       changeRegistration: function()
       {
         const self = this;
         const app = self.$f7;
-
         self.promiseWithAuth('acadadmin/course/registration/'+self.thisRegistration.type
-          , self.thisRegistration).then( function(x) {
-            console.log('Update registration: ', x.data);
+          , self.thisRegistration).then(function(x) {
+            var res = JSON.parse(x.data).data;
+            if(res.success) {
+              app.notification.create({title:"Success", subtitle:res.msg
+                , closeOnClick: true, closeTimeout: 3000}).open();
+              self.fetchRegistration();
+            }
+            else
+              app.notification.create({title:"Failed", subtitle:res.msg
+                , closeOnClick: true, closeTimeout: 10000}).open();
           });
+        self.openPopup = false;
+      },
+      addRegistration: function()
+      {
+        const self = this;
+        const app = self.$f7;
+        let course = self.thisCourseId.split('-');
+        self.thisRegistration.course_id = course[0];
+        self.thisRegistration.year = course[2];
+        self.thisRegistration.semester = course[1];
+        self.changeRegistration();
+      },
+      assignGrade: function()
+      {
+        const self = this;
+        const app = this.$f7;
+        console.log('Grading: ', self.thisRegistration);
+        self.promiseWithAuth('acadadmin/course/grade', self.thisRegistration)
+          .then(function(x) {
+            let res = JSON.parse(x.data).data;
+            if(res.success) {
+              app.notification.create({title:"Success", subtitle:res.msg
+                , closeOnClick: true, closeTimeout: 3000}).open();
+              self.fetchRegistration();
+            }
+            else
+              app.notification.create({title:"Failed", subtitle:res.msg
+                , closeOnClick: true, closeTimeout: 10000}).open();
+          });
+        self.openPopup = false;
       },
     }
   }
