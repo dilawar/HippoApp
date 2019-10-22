9aec677307d439579f905c5a2ff307031e180cb2
Dilawar Singh <dilawars@ncbs.res.in>
2019-10-23 05:13:36 +0530

    fixed JC.

--- src/main.js
+++ src/main.js
@@ -289,7 +289,7 @@ Vue.mixin({
       },
       getRoles: function() {
          const self = this;
-         if(self.$store.getters.profile)
+         if(self.$store.getters.profile.roles)
             return self.$store.getters.profile.roles.split(',');
          return 'USER';
       },
