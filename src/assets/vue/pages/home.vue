6d2263f675d09206e9bc6c5bc47e2df1d0182457
Dilawar Singh <dilawars@ncbs.res.in>
2019-10-24 02:50:40 +0530

    Added background image url.

--- src/assets/vue/pages/home.vue
+++ src/assets/vue/pages/home.vue
@@ -1,5 +1,5 @@
 <template>
-  <f7-page>
+  <f7-page class="with_photography_club">
     <f7-navbar>
       <f7-nav-left>
         <!-- LEFT PANEL -->
@@ -66,13 +66,11 @@
                       panel-close>
           <f7-icon slot="media" icon="fa fa-bullhorn fa-2x"></f7-icon>
         </f7-list-item>
-
       </f7-list>
     </f7-block>
-
-    <f7-block v-if="! isUserAuthenticated()">
+    <f7-block v-if="! isUserAuthenticated()" style="float:right;background:none;">
       <f7-list media-list no-hairlines>
-        <f7-list-item>
+        <f7-list-item style="background:none">
           <div slot="after">
             <font v-if="isHippoAlive">Hippo is alive. You may login.</font>
             <font v-else>
