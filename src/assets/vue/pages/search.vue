500366b917a46c28efdf390bee3b39e1d35bb8e6
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-01-05 20:25:22 +0530

    prepare for beta release.

--- src/assets/vue/pages/search.vue
+++ src/assets/vue/pages/search.vue
@@ -1,37 +1,43 @@
 <template>
-   <f7-page page-content>
-      <f7-navbar title="Search" back-link="Back"></f7-navbar>
-      <f7-block>
-         <f7-list media-list no-hairlines inset>
-            <f7-list-input @input="query=$event.target.value"
-                  type="text" 
-                  placeholder="Query e.g. Purchase, Chakrapani, 6532">
-            </f7-list-input>
-            <f7-button @click="searchIntranet" raised fill round>Search</f7-button>
-         </f7-list>
+  <f7-page page-content>
+    <f7-navbar title="Search" back-link="Back"></f7-navbar>
 
-      <f7-list media-list>
-         <f7-list-item v-for="(e,key) in ldapInfo" 
-                       :key="key"
-                       :after="e.extension"
-                       :footer="`${e.group}`"
-                       >
-                       <f7-link slot="title" 
-                                external
-                                icon="fa fa-envelope-o fa-fw"
-                                :href="`mailto:${e.email}`"
-                                target="_blank"
-                                > <small>{{e.name}}</small></f7-link>
-                       <f7-link slot="after" 
-                                external
-                                icon="fa fa-phone fa-fw"
-                                :href="`tel:${e.phone}`"
-                                target="_blank"
-                                > <small> {{e.phone}}</small> </f7-link>
-         </f7-list-item>
-      </f7-list>
-      </f7-block>
-   </f7-page>
+    <f7-list media-list no-hairlines inset>
+      <f7-list-input 
+             @input="query=$event.target.value"
+             @change="searchIntranet"
+             type="text" 
+             placeholder="Query e.g. Purchase, Chakrapani, 6532">
+      </f7-list-input>
+      <f7-button @click="searchIntranet" small raised>Search</f7-button>
+    </f7-list>
+
+    <f7-list media-list>
+      <f7-list-item v-for="(e,key) in ldapInfo" :key="key">
+        <div slot="media"></div>
+        <div slot="title">{{e.name}}</div>
+        <div slot="header">
+          <f7-link external
+                   icon="fa fa-envelope-o fa-fw"
+                   :href="`mailto:${e.email}`"
+                   target="_blank">
+            {{e.email}} 
+          </f7-link>
+          <f7-link external
+                   icon="fa fa-phone fa-fw"
+                   :href="`tel:${e.phone}`"
+                   target="_blank">
+            {{e.phone}}
+          </f7-link>
+        </div>
+        <div slot="footer" class="text-color-gray">
+          Group: {{e.group}}
+        </div>
+      </f7-list-item>
+      <f7-list-item></f7-list-item>
+    </f7-list>
+
+  </f7-page>
 </template>
 
 <script>
@@ -39,31 +45,35 @@ import moment from 'moment';
 moment.defaultFormat = 'YYYY-MM-DD';
 
 export default {
-   data() {
+  data() {
+    const self = this;
+    return {
+      query: null,
+      ldapCache: JSON.parse(self.$localStorage.get('ldap.cache', '[]')),
+      ldapInfo : [],
+    };
+  },
+  methods: { 
+    searchIntranet: function( )
+    {
       const self = this;
-      return {
-         query: null,
-         ldapCache: JSON.parse(self.$localStorage.get('ldap.cache', '[]')),
-         ldapInfo : [],
-      };
-   },
-   methods: { 
-      searchIntranet: function( )
-      {
-         const self = this;
-         const app = self.$f7;
-         self.promiseWithAuth('ldap/'+self.query)
-          .then( function(x) {
-           // Keep a copy else it will clutter.
-           self.ldapInfo =JSON.parse(x.data).data;
-           // Append to my personal library.
-           var stored = JSON.parse(self.$localStorage.get('ldap.cache'), '[]');
-           for(var k in stored)
-             ldap.push(stored[k]);
-           self.$localStorage.set('ldap.cache', JSON.stringify(ldap));
-         });
-      },
-   },
+      const app = self.$f7;
+      app.preloader.show();
+      var ldap = [];
+      self.promiseWithAuth('ldap/'+self.query)
+        .then( function(x) {
+          // Keep a copy else it will clutter.
+          self.ldapInfo =JSON.parse(x.data).data;
+          // Append to my personal library.
+          var stored = JSON.parse(self.$localStorage.get('ldap.cache'), '[]');
+          for(var k in stored)
+            ldap.push(stored[k]);
+          self.$localStorage.set('ldap.cache', JSON.stringify(ldap));
+          app.preloader.hide();
+        });
+      setTimeout( () => app.preloader.hide(), 1000);
+    },
+  },
 };
 
 </script>
