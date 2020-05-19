6bd55df381526ea2e8448ecc659bb8423f5138d4
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-19 17:13:37 +0530

    fetch `login` from vuex store.

--- src/assets/vue/pages/panel-left.vue
+++ src/assets/vue/pages/panel-left.vue
@@ -1,91 +1,81 @@
 <template>
-   <f7-page @page:init="refreshData" @page:refresh="refreshData">
-      <f7-navbar title="Hippo"></f7-navbar>
-      <!-- More information here -->
+  <f7-page @page:init="refreshData" @page:refresh="refreshData">
+    <f7-navbar title="Hippo"></f7-navbar>
+    <!-- More information here -->
 
-         <f7-block-title>Welcome {{username}}. </f7-block-title>
+    <f7-block-title>Welcome {{username}}. </f7-block-title>
 
-         <f7-list no-hairlines>
-            <f7-list-item link="/notifications/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="Notifications"
-                          panel-close>
-               <span slot="media" class="fa-stack" v-if="notifications.length>=0">
-                  <i class="fas fa-bell fa-stack-2x"></i>
-                  <i class="fas fa-stack circle-red">{{notifications.length}}</i>
-               </span>
-            </f7-list-item>
+    <f7-list no-hairlines>
+      <f7-list-item link="/notifications/" 
+        target="_blank"
+        view=".view-main"
+        title="Notifications"
+        panel-close>
+        <span slot="media" class="fa-stack" v-if="notifications.length>=0">
+          <i class="fas fa-bell fa-stack-2x"></i>
+          <i class="fas fa-stack circle-red">{{notifications.length}}</i>
+        </span>
+      </f7-list-item>
 
-            <!--
-            <f7-list-item link="/home/" 
-                          view=".view-main"
-                          title="Home" 
-                          panel-close
-                          >
-               <f7-icon slot="media" icon="fa fa-home fa-2x"></f7-icon>
-            </f7-list-item>
-            -->
+      <f7-list-item :link="'/profile/'+username" 
+        target="_blank"
+        view=".view-main"
+        title="My Profile" 
+        panel-close
+      >
+        <f7-icon slot="media" icon="fa fa-user fa-2x"></f7-icon>
+      </f7-list-item>
 
-            <f7-list-item :link="'/profile/'+username" 
-                          target="_blank"
-                          view=".view-main"
-                          title="My Profile" 
-                          panel-close
-                          >
-               <f7-icon slot="media" icon="fa fa-user fa-2x"></f7-icon>
-            </f7-list-item>
+      <f7-list-item link="/mybooking/" 
+        target="_blank"
+        view=".view-main"
+        title="My Bookings" 
+        panel-close
+      >
+        <f7-icon slot="media" icon="fa fa-edit fa-2x"></f7-icon>
+      </f7-list-item>
 
-            <f7-list-item link="/mybooking/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="My Bookings" 
-                          panel-close
-                          >
-               <f7-icon slot="media" icon="fa fa-edit fa-2x"></f7-icon>
-            </f7-list-item>
+      <f7-list-item v-if="profile.eligible_for_aws"
+        link="/myaws/" 
+        target="_blank"
+        view=".view-main"
+        title="My AWS" 
+        panel-close
+      >
+        <f7-icon slot="media" icon="fa fa-graduation-cap fa-2x"></f7-icon>
+      </f7-list-item>
 
-            <f7-list-item v-if="profile.eligible_for_aws"
-                          link="/myaws/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="My AWS" 
-                          panel-close
-                          >
-                <f7-icon slot="media" icon="fa fa-graduation-cap fa-2x"></f7-icon>
-            </f7-list-item>
+      <f7-list-item link="/myjc/" 
+        target="_blank"
+        view=".view-main"
+        title="Journal Clubs" 
+        panel-close
+      >
+        <f7-icon slot="media" icon="fa fa-users fa-2x"></f7-icon>
+      </f7-list-item>
 
-            <f7-list-item link="/myjc/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="Journal Clubs" 
-                          panel-close
-                          >
-                <f7-icon slot="media" icon="fa fa-users fa-2x"></f7-icon>
-            </f7-list-item>
-
-            <f7-list-item link="/mycourse/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="My Courses" 
-                          panel-close
-                          >
-             <f7-icon slot="media" icon="fa fa-book fa-2x"></f7-icon>
-            </f7-list-item>
+      <f7-list-item link="/mycourse/" 
+        target="_blank"
+        view=".view-main"
+        title="My Courses" 
+        panel-close
+      >
+        <f7-icon slot="media" icon="fa fa-book fa-2x"></f7-icon>
+      </f7-list-item>
 
 
-            <f7-list-item link="/myinventory/" 
-                          target="_blank"
-                          view=".view-main"
-                          title="Lab Inventory"
-                          panel-close
-                          >
-               <f7-icon slot="media" icon="fa fa-archive fa-2x"></f7-icon>
-            </f7-list-item>
+      <f7-list-item link="/myinventory/" 
+        target="_blank"
+        view=".view-main"
+        title="Lab Inventory"
+        panel-close
+      >
+        <f7-icon slot="media" icon="fa fa-archive fa-2x"></f7-icon>
+      </f7-list-item>
 
-         </f7-list>
+    </f7-list>
 
-   </f7-page>
+  </f7-page>
 </template>
 
 <script>
@@ -93,7 +83,7 @@ export default {
   data() {
     const self = this;
     return {
-      username: 'Guest',
+      username: self.$store.getters.profile.login,
       alreadyLoggedIn: false,
       profile: [],
       notifications: [],
@@ -103,7 +93,6 @@ export default {
   {
     const self = this;
     self.notifications = self.loadStore('notifications').filter(x =>false==x.is_read);
-    self.username = self.$localStorage.get('HIPPO-LOGIN');
     self.alreadyLoggedIn = self.isUserAuthenticated();
     self.postWithPromise('/me/profile').then(function(x) {
       self.profile = JSON.parse(x.data).data;
