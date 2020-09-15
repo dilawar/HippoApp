859c465b0eda2065a75fbb41134545b339cfa355
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-09-15 11:58:21 +0530

    check for PUBLIC in events type.

--- src/assets/vue/pages/smartbook.vue
+++ src/assets/vue/pages/smartbook.vue
@@ -27,9 +27,9 @@
           @change="changeSendEmailToAcademic"
           title="Send email(s) to Academic community">
           <div slot="text">
-            <strong>If cheched,</strong>, Hippo will send email
+            <strong>If cheched,</strong> Hippo will send email
             to academic mailing list on the day of the event. Also the event
-            will appear on the NCBS's Public Calendar.
+            will appear on the calendar.
           </div>
         </f7-list-item>
       </f7-list>
