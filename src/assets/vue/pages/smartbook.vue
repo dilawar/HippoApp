918806c1e8305cb9064b0551761c43c105d53c1f
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-10-01 23:17:39 +0530

    updated link.

--- src/assets/vue/pages/smartbook.vue
+++ src/assets/vue/pages/smartbook.vue
@@ -5,7 +5,7 @@
       <f7-link external target="_system"
         slot="right"
         icon="far fa-question-circle"
-        href="https://ncbs-hippo.readthedocs.io/en/latest/user_manual/#how-to-create-a-general-booking-request"
+        href="https://diilawar.github.io/hippoapp#How%20do%20I%20create%20a%20booking%20request%3F"
       >
         <tt>DOCS</tt>
       </f7-link>
