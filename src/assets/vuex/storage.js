d62bdd79ea547ff08e3bc50d05f5358f43d578b0
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-11-20 08:22:44 +0530

    Added ical url.

--- src/assets/vuex/storage.js
+++ src/assets/vuex/storage.js
@@ -10,7 +10,9 @@ export default new Vuex.Store({
       user: localStorage.getItem('HIPPO-LOGIN'),
       alreadyLoggedIn: false,
       // api : 'https://ncbs.res.in/hippo/v1/api',
+      // icalurl : 'https://ncbs.res.in/hippo/v1/pub/ical/
       api : 'http://172.16.223.30/hippo/v1/api',
+      icalurl : 'http://172.16.223.30/hippo/v1/pub/ical/',
       apikey : localStorage.getItem('HIPPO-API-KEY'),
       apiKeyGMap: '',
       tobook: null,
