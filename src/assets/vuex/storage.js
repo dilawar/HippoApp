892d9e3b2a05c161523eb3ec42de0d383bb782fd
Dilawar Singh <dilawars@ncbs.res.in>
2021-04-23 11:47:36 +0530

    Updated API.

--- src/assets/vuex/storage.js
+++ src/assets/vuex/storage.js
@@ -9,8 +9,8 @@ export default new Vuex.Store({
    state: {
       user: localStorage.getItem('HIPPO-LOGIN'),
       alreadyLoggedIn: false,
-      api : 'https://ncbs.res.in/hippo/v1/api',
-      icalurl : 'https://ncbs.res.in/hippo/v1/pub/ical/',
+      api : 'https://www.ncbs.res.in/hippo/v1/api',
+      icalurl : 'https://www.ncbs.res.in/hippo/v1/pub/ical/',
       // api : 'http://172.16.223.30/hippo/v1/api',
       // icalurl : 'http://172.16.223.30/hippo/v1/pub/ical/',
       apikey : localStorage.getItem('HIPPO-API-KEY'),
